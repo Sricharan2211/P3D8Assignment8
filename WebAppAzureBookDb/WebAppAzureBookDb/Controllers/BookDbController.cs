f7ff4897f19efd86c31797beeffa6ae5777229f2
Sricharan2211 <tudugani.charan@mphasis.com>
2024-01-16 23:17:47 +0530

    Phase 4 assignment 8

---
+++ WebAppAzureBookDb/WebAppAzureBookDb/Controllers/BookDbController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAppAzureBookDb.Models;
+
+namespace WebAppAzureBookDb.Controllers
+{
+    public class BookDbController : Controller
+    {
+        private readonly BookStoreDbContext _context;
+
+        public BookDbController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BookDb
+        public async Task<IActionResult> Index()
+        {
+              return _context.BookDbs != null ? 
+                          View(await _context.BookDbs.ToListAsync()) :
+                          Problem("Entity set 'BookStoreDbContext.BookDbs'  is null.");
+        }
+
+        // GET: BookDb/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.BookDbs == null)
+            {
+                return NotFound();
+            }
+
+            var bookDb = await _context.BookDbs
+                .FirstOrDefaultAsync(m => m.Bid == id);
+            if (bookDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookDb);
+        }
+
+        // GET: BookDb/Create
+        public IActionResult Create()
+        {
+            return View(new BookDb());
+        }
+
+        // POST: BookDb/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Bid,Author,Publisher,Category,Price")] BookDb bookDb)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bookDb);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookDb);
+        }
+
+        // GET: BookDb/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.BookDbs == null)
+            {
+                return NotFound();
+            }
+
+            var bookDb = await _context.BookDbs.FindAsync(id);
+            if (bookDb == null)
+            {
+                return NotFound();
+            }
+            return View(bookDb);
+        }
+
+        // POST: BookDb/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Bid,Author,Publisher,Category,Price")] BookDb bookDb)
+        {
+            if (id != bookDb.Bid)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bookDb);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookDbExists(bookDb.Bid))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookDb);
+        }
+
+        // GET: BookDb/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.BookDbs == null)
+            {
+                return NotFound();
+            }
+
+            var bookDb = await _context.BookDbs
+                .FirstOrDefaultAsync(m => m.Bid == id);
+            if (bookDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookDb);
+        }
+
+        // POST: BookDb/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.BookDbs == null)
+            {
+                return Problem("Entity set 'BookStoreDbContext.BookDbs'  is null.");
+            }
+            var bookDb = await _context.BookDbs.FindAsync(id);
+            if (bookDb != null)
+            {
+                _context.BookDbs.Remove(bookDb);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookDbExists(int id)
+        {
+          return (_context.BookDbs?.Any(e => e.Bid == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebAppAzureBookDb/WebAppAzureBookDb/Models/BookStoreDbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAppAzureBookDb.Models
+{
+    public partial class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext()
+        {
+        }
+
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<BookDb> BookDbs { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:sricharan2205.database.windows.net,1433;Initial Catalog=BookStoreDb;Persist Security Info=False;User ID=charan;Password=Krishna22;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookDb>(entity =>
+            {
+                entity.HasKey(e => e.Bid)
+                    .HasName("PK__BookDb__C6DE0CC1B7582710");
+
+                entity.ToTable("BookDb");
+
+                entity.Property(e => e.Bid)
+                    .ValueGeneratedNever()
+                    .HasColumnName("BId");
+
+                entity.Property(e => e.Author).HasMaxLength(50);
+
+                entity.Property(e => e.Category).HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnName("price");
+
+                entity.Property(e => e.Publisher).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
